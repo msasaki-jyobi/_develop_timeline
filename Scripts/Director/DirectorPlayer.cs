44fccb8b5b491e658a9e45821f3617d267a12a8e
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-10-03 10:48:34 +0900

    Update Demo完成（Prefab毎に作る仕様）

--- Scripts/Director/DirectorPlayer.cs
+++ Scripts/Director/DirectorPlayer.cs
@@ -10,14 +10,12 @@ using UnityEngine.Rendering.PostProcessing;
 
 namespace develop_timeline
 {
-    public class DirectorManager : SingletonMonoBehaviour<DirectorManager>
+    public class DirectorPlayer : SingletonMonoBehaviour<DirectorPlayer>
     {
-        // Inspector attach 
-        public PlayableDirector DefaultDirector;
-        public CinemachineBrain Brain;
-        public Animator PosA;
-        public Animator PosB;
-        public Animator DefaultUnitA;
+        [Space(10)]
+        public PlayableDirector Director;
+        public Animator PositionA;
+        public Animator PositionB;
 
         [Space(10)]
         // 逆再生用
@@ -28,33 +26,31 @@ namespace develop_timeline
 
         [Space(10)]
         // Debug
-        public PlayableAsset DebugPlayable;
-        public Animator DebugUnitB;
-        public bool IsDebugPlay;
-        public bool IsPlayerControl;
+        public bool IsPlayableControl;
 
-        private PlayableDirector _playingDirector;
+        private CinemachineBrain _brain;
+
 
         void Update()
         {
             // Debug
-            if (IsDebugPlay)
-            {
-                if (Input.GetKeyDown(KeyCode.Alpha1))
-                    OnSetPlayDirector(DebugPlayable, unitA: DefaultUnitA, unitB: DebugUnitB, overrideDirector: null);
-                if (Input.GetKeyDown(KeyCode.Alpha2))
-                    OnSetPlayDirector(DebugPlayable, unitA: DefaultUnitA, unitB: DebugUnitB, overrideDirector: null, playPosition: new Vector3(0, 10, 0), playRotation: new Vector3(0, 45, 0));
-            }
+            //if (IsDebugPlay)
+            //{
+            //    if (Input.GetKeyDown(KeyCode.Alpha1))
+            //        OnSetPlayDirector(DebugPlayable, unitA: DefaultUnitA, unitB: DebugUnitB, overrideDirector: null);
+            //    if (Input.GetKeyDown(KeyCode.Alpha2))
+            //        OnSetPlayDirector(DebugPlayable, unitA: DefaultUnitA, unitB: DebugUnitB, overrideDirector: null, playPosition: new Vector3(0, 10, 0), playRotation: new Vector3(0, 45, 0));
+            //}
 
 
-            if (_playingDirector == null) return;
+            if (Director == null) return;
 
-            if (!IsPlayerControl) return;
+            if (!IsPlayableControl) return;
             // Init Play
             if (Input.GetKeyDown(KeyCode.F))
             {
-                _playingDirector.time = 0.01f;
-                _playingDirector.Play();
+                Director.time = 0.01f;
+                Director.Play();
             }
             // Back Play
             if (Input.GetKeyDown(KeyCode.A))
@@ -70,99 +66,83 @@ namespace develop_timeline
             //{
             //    IsReversing = !IsReversing;
             //}
-            if (_playingDirector != null)
-                if (_playingDirector.time < 0.01f)
+            if (Director != null)
+                if (Director.time < 0.01f)
                 {
-                    if (_playingDirector.state == PlayState.Playing)
+                    if (Director.state == PlayState.Playing)
                     {
-                        _playingDirector.time = 0.01f;
-                        _playingDirector.playableGraph.GetRootPlayable(0).SetSpeed(0);
+                        Director.time = 0.01f;
+                        Director.playableGraph.GetRootPlayable(0).SetSpeed(0);
                     }
                 }
         }
 
         public void ChangePlaySpeed(float newSpeed, float duration)
         {
-            float currentSpeed = (float)_playingDirector.playableGraph.GetRootPlayable(0).GetSpeed();
-            DOTween.To(() => currentSpeed, x =>
+            float currentSpeed = (float)Director.playableGraph.GetRootPlayable(0).GetSpeed();
+            DOTween.To(() => currentSpeed, (DG.Tweening.Core.DOSetter<float>)(x =>
             {
-                _playingDirector.playableGraph.GetRootPlayable(0).SetSpeed(x);
+                PlayableExtensions.SetSpeed<Playable>(this.Director.playableGraph.GetRootPlayable(0), x);
                 currentSpeed = x;
-            }, newSpeed, duration);
+            }), newSpeed, duration);
         }
 
         /// <summary>
         /// ムービーを実行する
         /// </summary>
-        /// <param name="asset">実行するムービー</param>
+        /// <param name="Asset">実行するムービー</param>
         /// <param name="unitB">敵のAnimator</param>
         /// <param name="playerParent">プレイヤーの位置管理オブジェクト</param>
         /// <param name="enemyParent">敵の位置管理オブジェクト</param>
-        public void OnSetPlayDirector(PlayableAsset asset, Animator unitA = null, Animator unitB = null, PlayableDirector overrideDirector = null, Vector3 playPosition = default, Vector3 playRotation = default)
+        public void OnSetPlayDirector(Animator unitA = null, Animator unitB = null)
         {
             var unitATrackName = "UnitA";
             var unitBTrackName = "UnitB";
             var posATrackName = "PosA";
             var posBTrackName = "PosB";
 
-            // Direcotr Override
-            if (overrideDirector != null)
-                _playingDirector = overrideDirector;
-            else if (_playingDirector == null)
-                _playingDirector = DefaultDirector;
-
-            // Play Pos Rot
-            if (playPosition != default)
-                transform.position = playPosition;
-            if (playRotation != default)
-                transform.rotation = Quaternion.Euler(playRotation);
-
-            // PlayerAnimator
-            if (unitA == null)
-                unitA = DefaultUnitA;
+            _brain = Camera.main.GetComponent<CinemachineBrain>();
 
             // Bind Pos
-            if (PosA != null)
-                BindAnimatorTrackDirector(_playingDirector, asset, posATrackName, PosA);
-            if (PosB != null)
-                BindAnimatorTrackDirector(_playingDirector, asset, posBTrackName, PosB);
+            if (PositionA != null)
+                BindAnimatorTrackDirector(Director, Director.playableAsset, posATrackName, PositionA);
+            if (PositionB != null)
+                BindAnimatorTrackDirector(Director, Director.playableAsset, posBTrackName, PositionB);
 
             // UnitA
             if (unitA != null)
             {
-                if (PosA != null)
+                if (PositionA != null)
                 {
                     // offsetどうする？キャラ大きいとか小さいとか
-                    unitA.transform.parent = PosA.transform;//親を設定
+                    unitA.transform.parent = PositionA.transform;//親を設定
                     unitA.transform.localPosition = Vector3.zero; // 座標を親に合わせる
                     unitA.transform.rotation = Quaternion.Euler(Vector3.zero); // 向きを親に合わせる
                 }
                 // Bind UnitA
-                BindAnimatorTrackDirector(_playingDirector, asset, unitATrackName, unitA);
+                BindAnimatorTrackDirector(Director, Director.playableAsset, unitATrackName, unitA);
             }
 
             // UnitB 
             if (unitB != null)
             {
-                if (PosB != null)
+                if (PositionB != null)
                 {
-                    unitB.transform.parent = PosB.gameObject.transform;
+                    unitB.transform.parent = PositionB.gameObject.transform;
                     unitB.transform.localPosition = Vector3.zero; // 座標を親に合わせる
                     unitB.transform.rotation = Quaternion.Euler(Vector3.zero); // 向きを親に合わせる
                 }
                 // Bind UnitB
-                BindAnimatorTrackDirector(_playingDirector, asset, unitBTrackName, unitB);
+                BindAnimatorTrackDirector(Director, Director.playableAsset, unitBTrackName, unitB);
             }
 
-
-
             // Cinemachine
-            var brainTrack = _playingDirector.playableAsset.outputs.First(c => c.streamName == "Brain");
-            _playingDirector.SetGenericBinding(brainTrack.sourceObject, Brain);
+            var brainTrack = Director.playableAsset.outputs.First(c => c.streamName == "Brain");
+            Director.SetGenericBinding(brainTrack.sourceObject, _brain);
 
             // DirectorPlay
-            _playingDirector.time = 0.01f;
-            _playingDirector.Play();
+            Director.time = 0.01f;
+            Director.Play();
             //DebugPlayable.Instance.ChangeSpeed(1, 0.05f);
         }
         /// <summary>
---
+++ Scripts/Director/Sample/SampleDirectorPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace develop_timeline
+{
+    public class SampleDirectorPlayer : MonoBehaviour
+    {
+        public DirectorPlayer PlayablePrefab;
+        public Animator UnitA;
+        public Animator UnitB;
+
+        private void Update()
+        {
+            // Debug
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                GameObject director = Instantiate(PlayablePrefab.gameObject, transform.position, Quaternion.identity);
+                if (director.TryGetComponent<DirectorPlayer>(out var directorPlayer))
+                    directorPlayer.OnSetPlayDirector(unitA: UnitA, unitB: UnitB);
+
+                // îjä¸Ç«Ç∑ÇÈÅH
+            }
+
+        }
+    }
+
+}
