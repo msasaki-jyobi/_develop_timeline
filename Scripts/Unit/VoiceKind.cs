5c2b3d1257bbc55f7eaaa16dfaac210da456662d
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-10-15 02:21:12 +0900

    Update 1015

---
+++ Scripts/Unit/VoiceKind.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace develop_common
+{
+    [Serializable]
+    public class VoiceKind
+    {
+        public string ClipName;
+        public ClipData ClipData;
+    }
+}
